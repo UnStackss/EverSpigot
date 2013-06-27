b9352b2111d5418a8af5e893df8161a4326aa37f
md_5 <git@md-5.net>
2013-06-27 17:26:09 +1000

    Properly Close Inventories
    Properly close inventories when unloading and switching worlds.

--- src/main/java/net/minecraft/server/level/WorldServer.java
+++ src/main/java/net/minecraft/server/level/WorldServer.java
@@ -1127,6 +1127,15 @@ public class WorldServer extends World implements GeneratorAccessSeed {
     }
 
     public void unload(Chunk chunk) {
+        // Spigot Start
+        for (net.minecraft.world.level.block.entity.TileEntity tileentity : chunk.getBlockEntities().values()) {
+            if (tileentity instanceof net.minecraft.world.IInventory) {
+                for (org.bukkit.entity.HumanEntity h : Lists.newArrayList(((net.minecraft.world.IInventory) tileentity).getViewers())) {
+                    h.closeInventory();
+                }
+            }
+        }
+        // Spigot End
         chunk.clearAllBlockEntities();
         chunk.unregisterTickContainerFromLevel(this);
     }
@@ -2031,6 +2040,13 @@ public class WorldServer extends World implements GeneratorAccessSeed {
 
         public void onTrackingEnd(Entity entity) {
             org.spigotmc.AsyncCatcher.catchOp("entity unregister"); // Spigot
+            // Spigot Start
+            if (entity.getBukkitEntity() instanceof org.bukkit.inventory.InventoryHolder && (!(entity instanceof EntityPlayer) || entity.getRemovalReason() != Entity.RemovalReason.KILLED)) { // SPIGOT-6876: closeInventory clears death message
+                for (org.bukkit.entity.HumanEntity h : Lists.newArrayList(((org.bukkit.inventory.InventoryHolder) entity.getBukkitEntity()).getInventory().getViewers())) {
+                    h.closeInventory();
+                }
+            }
+            // Spigot End
             WorldServer.this.getChunkSource().removeEntity(entity);
             if (entity instanceof EntityPlayer entityplayer) {
                 WorldServer.this.players.remove(entityplayer);
