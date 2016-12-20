c1a228e148c04f937d1120a62d71678729ce3513
Aikar <aikar@aikar.co>
2016-12-20 15:26:27 -0500

    Configurable Cartographer Treasure Maps
    Allow configuring for cartographers to return the same map location
    
    Also allow turning off treasure maps all together as they can eat up Map ID's
    which are limited in quantity.

--- src/main/java/net/minecraft/world/entity/npc/VillagerTrades.java
+++ src/main/java/net/minecraft/world/entity/npc/VillagerTrades.java
@@ -1828,7 +1828,8 @@ public class VillagerTrades {
                 return null;
             } else {
                 ServerLevel serverLevel = (ServerLevel)entity.level();
-                BlockPos blockPos = serverLevel.findNearestMapStructure(this.destination, entity.blockPosition(), 100, true);
+                if (!serverLevel.paperConfig().environment.treasureMaps.enabled) return null; // Paper - Configurable cartographer treasure maps
+                BlockPos blockPos = serverLevel.findNearestMapStructure(this.destination, entity.blockPosition(), 100, !serverLevel.paperConfig().environment.treasureMaps.findAlreadyDiscoveredVillager); // Paper - Configurable cartographer treasure maps
                 if (blockPos != null) {
                     ItemStack itemStack = MapItem.create(serverLevel, blockPos.getX(), blockPos.getZ(), (byte)2, true, true);
                     MapItem.renderBiomePreviewMap(serverLevel, itemStack);
