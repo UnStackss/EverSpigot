a86797fd5b1864b9b27f40b25c6340afd7a87126
Aikar <aikar@aikar.co>
2018-03-11 14:13:33 -0400

    Disable Explicit Network Manager Flushing
    This seems completely pointless, as packet dispatch uses .writeAndFlush.
    
    Things seem to work fine without explicit flushing, but incase issues arise,
    provide a System property to re-enable it using improved logic of doing the
    flushing on the netty event loop, so it won't do the flush on the main thread.
    
    Renable flushing by passing -Dpaper.explicit-flush=true

--- src/main/java/net/minecraft/network/Connection.java
+++ src/main/java/net/minecraft/network/Connection.java
@@ -123,6 +123,7 @@ public class Connection extends SimpleChannelInboundHandler<Packet<?>> {
     // Paper start - NetworkClient implementation
     public int protocolVersion;
     public java.net.InetSocketAddress virtualHost;
+    private static boolean enableExplicitFlush = Boolean.getBoolean("paper.explicit-flush"); // Paper - Disable explicit network manager flushing
     // Paper end
 
     // Paper start - add utility methods
@@ -457,7 +458,7 @@ public class Connection extends SimpleChannelInboundHandler<Packet<?>> {
         }
 
         if (this.channel != null) {
-            this.channel.flush();
+            if (enableExplicitFlush) this.channel.eventLoop().execute(() -> this.channel.flush()); // Paper - Disable explicit network manager flushing; we don't need to explicit flush here, but allow opt in incase issues are found to a better version
         }
 
         if (this.tickCount++ % 20 == 0) {
