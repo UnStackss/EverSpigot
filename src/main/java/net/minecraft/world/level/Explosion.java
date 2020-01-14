3497b8d73c4326401d7e128f8862b4f7d2b898eb
BrodyBeckwith <brody@beckwith.dev>
2020-01-14 17:49:03 -0500

    Optimize call to getFluid for explosions

--- src/main/java/net/minecraft/world/level/Explosion.java
+++ src/main/java/net/minecraft/world/level/Explosion.java
@@ -193,7 +193,7 @@ public class Explosion {
                         for (float f1 = 0.3F; f > 0.0F; f -= 0.22500001F) {
                             BlockPos blockposition = BlockPos.containing(d4, d5, d6);
                             BlockState iblockdata = this.level.getBlockState(blockposition);
-                            FluidState fluid = this.level.getFluidState(blockposition);
+                            FluidState fluid = iblockdata.getFluidState(); // Paper - Perf: Optimize call to getFluid for explosions
 
                             if (!this.level.isInWorldBounds(blockposition)) {
                                 break;
