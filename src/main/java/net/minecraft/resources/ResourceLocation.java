783af9679c9e41eae895f2f91ac94d03abebc089
Nassim Jahnke <nassim@njahnke.dev>
2021-08-26 12:09:47 +0200

    Sanitize ResourceLocation error logging

--- src/main/java/net/minecraft/resources/ResourceLocation.java
+++ src/main/java/net/minecraft/resources/ResourceLocation.java
@@ -247,7 +247,7 @@ public final class ResourceLocation implements Comparable<ResourceLocation> {
 
     private static String assertValidNamespace(String namespace, String path) {
         if (!isValidNamespace(namespace)) {
-            throw new ResourceLocationException("Non [a-z0-9_.-] character in namespace of location: " + namespace + ":" + path);
+            throw new ResourceLocationException("Non [a-z0-9_.-] character in namespace of location: " + org.apache.commons.lang3.StringUtils.normalizeSpace(namespace) + ":" + org.apache.commons.lang3.StringUtils.normalizeSpace(path)); // Paper - Sanitize ResourceLocation error logging
         } else {
             return namespace;
         }
@@ -268,7 +268,7 @@ public final class ResourceLocation implements Comparable<ResourceLocation> {
 
     private static String assertValidPath(String namespace, String path) {
         if (!isValidPath(path)) {
-            throw new ResourceLocationException("Non [a-z0-9/._-] character in path of location: " + namespace + ":" + path);
+            throw new ResourceLocationException("Non [a-z0-9/._-] character in path of location: " + namespace + ":" + org.apache.commons.lang3.StringUtils.normalizeSpace(path)); // Paper - Sanitize ResourceLocation error logging
         } else {
             return path;
         }
