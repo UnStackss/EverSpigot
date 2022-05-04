b036bce013de5858cfa1423761d43f39ab2b1278
u9g <git@u9g.dev>
2022-05-03 20:41:37 -0400

    Add PlayerStopUsingItemEvent

--- src/main/java/net/minecraft/world/entity/LivingEntity.java
+++ src/main/java/net/minecraft/world/entity/LivingEntity.java
@@ -4073,6 +4073,7 @@ public abstract class LivingEntity extends Entity implements Attackable {
 
     public void releaseUsingItem() {
         if (!this.useItem.isEmpty()) {
+            if (this instanceof ServerPlayer) new io.papermc.paper.event.player.PlayerStopUsingItemEvent((Player) getBukkitEntity(), useItem.asBukkitMirror(), getTicksUsingItem()).callEvent(); // Paper - Add PlayerStopUsingItemEvent
             this.useItem.releaseUsing(this.level(), this, this.getUseItemRemainingTicks());
             if (this.useItem.useOnRelease()) {
                 this.updatingUsingItem();
