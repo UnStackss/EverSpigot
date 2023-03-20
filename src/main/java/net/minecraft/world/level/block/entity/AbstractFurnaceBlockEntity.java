e71b335859500e50e12065ce68e61f750b2d11ff
Jake Potrebic <jake.m.potrebic@gmail.com>
2023-03-19 20:36:22 -0700

    Fix crash relating to bad recipes in furnace-like tile entities

--- src/main/java/net/minecraft/world/level/block/entity/AbstractFurnaceBlockEntity.java
+++ src/main/java/net/minecraft/world/level/block/entity/AbstractFurnaceBlockEntity.java
@@ -616,6 +616,7 @@ public abstract class AbstractFurnaceBlockEntity extends BaseContainerBlockEntit
             Entry<ResourceLocation> entry = (Entry) objectiterator.next();
 
             worldserver.getRecipeManager().byKey((ResourceLocation) entry.getKey()).ifPresent((recipeholder) -> {
+                if (!(recipeholder.value() instanceof AbstractCookingRecipe)) return; // Paper - don't process non-cooking recipes
                 list.add(recipeholder);
                 AbstractFurnaceBlockEntity.createExperience(worldserver, vec3d, entry.getIntValue(), ((AbstractCookingRecipe) recipeholder.value()).getExperience(), blockposition, entityplayer, itemstack, amount); // CraftBukkit
             });
