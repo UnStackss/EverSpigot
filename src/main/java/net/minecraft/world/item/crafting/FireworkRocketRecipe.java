16fce2b44863166b9424055cc9d707ccec196225
Redned <redned235@gmail.com>
2023-06-12 11:54:12 -0500

    Fix incorrect crafting result amount for fireworks
    Although vanilla does not specifically call this method anywhere, this fixes a bug where the result using the Bukkit API returns the wrong amount.

--- src/main/java/net/minecraft/world/item/crafting/FireworkRocketRecipe.java
+++ src/main/java/net/minecraft/world/item/crafting/FireworkRocketRecipe.java
@@ -77,7 +77,7 @@ public class FireworkRocketRecipe extends CustomRecipe {
 
     @Override
     public ItemStack getResultItem(HolderLookup.Provider registriesLookup) {
-        return new ItemStack(Items.FIREWORK_ROCKET);
+        return new ItemStack(Items.FIREWORK_ROCKET, 3); // Paper - Fix incorrect crafting result amount
     }
 
     @Override
