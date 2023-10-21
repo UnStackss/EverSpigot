3b3e2337021d4b980bc771173da104ca9eb794db
Brokkonaut <hannos17@gmx.de>
2023-10-21 20:52:57 +0100

    Don't check if we can see non-visible entities

--- src/main/java/net/minecraft/server/level/ChunkMap.java
+++ src/main/java/net/minecraft/server/level/ChunkMap.java
@@ -1537,7 +1537,7 @@ public class ChunkMap extends ChunkStorage implements ChunkHolder.PlayerProvider
                 // Paper end - Configurable entity tracking range by Y
 
                 // CraftBukkit start - respect vanish API
-                if (!player.getBukkitEntity().canSee(this.entity.getBukkitEntity())) {
+                if (flag && !player.getBukkitEntity().canSee(this.entity.getBukkitEntity())) { // Paper - only consider hits
                     flag = false;
                 }
                 // CraftBukkit end
