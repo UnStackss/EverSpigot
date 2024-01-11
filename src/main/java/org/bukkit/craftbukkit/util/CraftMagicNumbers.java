ce204d38368d95bdd1e02af80ca504a42fa2dcef
Luis <luisc99@icloud.com>
2024-01-11 19:58:23 +0100

    Add api for spawn egg texture colors

--- src/main/java/org/bukkit/craftbukkit/util/CraftMagicNumbers.java
+++ src/main/java/org/bukkit/craftbukkit/util/CraftMagicNumbers.java
@@ -637,6 +637,15 @@ public final class CraftMagicNumbers implements UnsafeValues {
         return CraftRegistry.get(registry, namespacedKey, ApiVersion.CURRENT);
     }
 
+    // Paper start - spawn egg color visibility
+    @Override
+    public org.bukkit.Color getSpawnEggLayerColor(final EntityType entityType, final int layer) {
+        final net.minecraft.world.entity.EntityType<?> nmsType = org.bukkit.craftbukkit.entity.CraftEntityType.bukkitToMinecraft(entityType);
+        final net.minecraft.world.item.SpawnEggItem eggItem = net.minecraft.world.item.SpawnEggItem.byId(nmsType);
+        return eggItem == null ? null : org.bukkit.Color.fromRGB(eggItem.getColor(layer));
+    }
+    // Paper end - spawn egg color visibility
+
     /**
      * This helper class represents the different NBT Tags.
      * <p>
