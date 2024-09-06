PaperMC/Paper

LootTable API and replenishable lootables
Provides an API to control the loot table for an object.
Also provides a feature that any Lootable Inventory (Chests in Structures)
can automatically replenish after a given time.

This feature is good for long term worlds so that newer players
do not suffer with "Every chest has been looted"

== AT ==
public org.bukkit.craftbukkit.block.CraftBlockEntityState getTileEntity()Lnet/minecraft/world/level/block/entity/BlockEntity;
public org.bukkit.craftbukkit.block.CraftLootable setLootTable(Lorg/bukkit/loot/LootTable;J)V
public org.bukkit.craftbukkit.entity.CraftMinecartContainer setLootTable(Lorg/bukkit/loot/LootTable;J)V

paper-server/src/main/java/com/destroystokyo/paper/loottable/PaperLootableEntity.java
@@ -1,0 +1,29 @@
+package com.destroystokyo.paper.loottable;
+
+import net.minecraft.world.entity.vehicle.ContainerEntity;
+import org.bukkit.craftbukkit.CraftLootTable;
+import org.bukkit.loot.LootTable;
+import org.bukkit.loot.Lootable;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+public interface PaperLootableEntity extends Lootable {
+
+    ContainerEntity getHandle();
+
+    /* Lootable */
+    @Override
+    default @Nullable LootTable getLootTable() {
+        return CraftLootTable.minecraftToBukkit(this.getHandle().getContainerLootTable());
+    }
+
+    @Override
+    default void setLootTable(final @Nullable LootTable table, final long seed) {
+        this.getHandle().setContainerLootTable(CraftLootTable.bukkitToMinecraft(table));
+        this.getHandle().setContainerLootTableSeed(seed);
+    }
+
+    @Override
+    default long getSeed() {
+        return this.getHandle().getContainerLootTableSeed();
+    }
+}

UnStackss/EverSpigot

src/main/java/com/destroystokyo/paper/loottable/PaperLootableEntity.java
@@ -1,0 +1,29 @@
+package com.destroystokyo.paper.loottable;
+
+import net.minecraft.world.entity.vehicle.ContainerEntity;
+import org.bukkit.craftbukkit.CraftLootTable;
+import org.bukkit.loot.LootTable;
+import org.bukkit.loot.Lootable;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+public interface PaperLootableEntity extends Lootable {
+
+    ContainerEntity getHandle();
+
+    /* Lootable */
+    @Override
+    default @Nullable LootTable getLootTable() {
+        return CraftLootTable.minecraftToBukkit(this.getHandle().getLootTable());
+    }
+
+    @Override
+    default void setLootTable(final @Nullable LootTable table, final long seed) {
+        this.getHandle().setLootTable(CraftLootTable.bukkitToMinecraft(table));
+        this.getHandle().setLootTableSeed(seed);
+    }
+
+    @Override
+    default long getSeed() {
+        return this.getHandle().getLootTableSeed();
+    }
+}